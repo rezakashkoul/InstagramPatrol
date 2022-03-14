2b53e2db240491765807d2547dff26521cf20b99
Reza Kashkoul <rezakashkoul@gmail.com>
2022-03-14 16:12:01 +0330

    Initial Commit

---
+++ patInsta/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  patInsta
+//
+//  Created by Reza Kashkoul on 23-Esfand-1400 .
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
